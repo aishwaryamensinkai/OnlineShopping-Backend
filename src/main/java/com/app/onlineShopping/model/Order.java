a2639f7ac942eb277f2f4d518f44c80e193c66e7
Aishwarya Mensinkai <aishwaryamensinkai@gmail.com>
2024-04-13 18:33:15 -0400

    updated the packages

--- src/main/java/com/app/onlineShopping/model/Order.java
+++ src/main/java/com/app/onlineShopping/model/Order.java
@@ -1,14 +1,10 @@
 package com.app.onlineShopping.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
 
 @Data
 @Entity
